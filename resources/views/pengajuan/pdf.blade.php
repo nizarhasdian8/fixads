ed991b8460b7665e4a6fc9d1e733820b9f272c26
nizarhasdian8 <nizar.hasdian8@gmail.com>
2026-08-22 21:48:42 +0700

    permintaan bahan

--- resources/views/pengajuan/pdf.blade.php
+++ resources/views/pengajuan/pdf.blade.php
@@ -12,17 +12,43 @@
         th, td { border: 1px solid #ccc; padding: 6px; text-align: left; }
         th { background-color: #f5f5f5; font-weight: bold; }
         .text-center { text-align: center; }
-        .mt-20 { margin-top: 20px; }
+        .header-table { width: 100%; border: none; margin-bottom: 0; }
+        .header-table td { border: none; padding: 0; vertical-align: middle; }
+        .header-line { border-bottom: 3px solid #000; margin-top: 10px; margin-bottom: 20px; }
+        .signature-box { margin-top: 50px; width: 100%; text-align: right; }
+        .signature-space { height: 80px; }
     </style>
 </head>
 <body>
-    <h1>Fix Advertising</h1>
-    <h2>Laporan Permintaan Bahan Baku
-        @if($bulan) <br> Bulan {{ $namaBulan[(int)$bulan] }} @endif
+    {{-- KOP SURAT --}}
+    <table class="header-table">
+        <tr>
+            <td style="width: 30%; text-align: left;">
+                <img src="{{ public_path('assets/img/Logo Fix Advertising.png') }}" alt="Logo" style="width: 90px; height: auto;">
+            </td>
+            <td style="width: 40%; text-align: center;">
+                <h2 style="margin: 0; font-size: 20px; font-weight: bold;">Fix Advertising</h2>
+                <p style="margin: 0; font-size: 11px; color: #555;">
+                    Ruko Cipta Pesona Jl. Raya Cipamokolan No. 12,<br>
+                    Cipamokolan, Kecamatan Rancasari, Kota Bandung, Jawa Barat
+                </p>
+            </td>
+            <td style="width: 30%;">
+                {{-- KOLOM KOSONG UNTUK KESEIMBANGGAN --}}
+            </td>
+        </tr>
+    </table>
+    <div class="header-line"></div>
+
+    {{-- JUDUL LAPORAN --}}
+    <h1>Laporan Permintaan Bahan Baku</h1>
+    <h2>
+        @if($bulan) Bulan {{ $namaBulan[(int)$bulan] }} @endif
         @if($tahun) Tahun {{ $tahun }} @endif
         @if($status) (Status: {{ ucfirst($status) }}) @endif
     </h2>
 
+    {{-- TABEL DATA --}}
     <table>
         <thead>
             <tr>
@@ -42,7 +68,7 @@
                 <td>{{ $item->nomor_permintaan }}</td>
                 <td>{{ \Carbon\Carbon::parse($item->created_at)->format('d M Y') }}</td>
                 <td>{{ $item->bahanBaku->nama }}</td>
-                <td>{{ $item->jumlah }} {{ $item->bahanBaku->satuan }}</td>
+                <td>{{ floatval($item->jumlah) }} {{ $item->bahanBaku->satuan }}</td>
                 <td>{{ $item->pengaju->name }}</td>
                 <td>{{ $item->statusLabel() }}</td>
             </tr>
@@ -54,8 +80,13 @@
         </tbody>
     </table>
 
-    <div class="mt-20">
-        <p>Dicetak pada: {{ date('d M Y, H:i') }}</p>
+    {{-- TANDA TANGAN --}}
+    <div class="signature-box">
+        <p>Bandung, {{ date('d M Y') }}</p>
+        <p>Hormat Kami,</p>
+        <div class="signature-space"></div>
+        <p style="text-decoration: underline; font-weight: bold;">(Nama CIO Marketing)</p>
+        <p>CIO Marketing</p>
     </div>
 </body>
 </html>
